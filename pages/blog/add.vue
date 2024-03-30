c0c664d67c34201171d891df8cd42fe44bdde7ab
tzptzptzptzptzp <git@tzp.pink>
2024-03-31 00:06:04 +0900

    ✨ 投稿機能を実装

--- pages/blog/add.vue
+++ pages/blog/add.vue
@@ -1,14 +1,42 @@
+<script setup lang="ts">
+import axios from "axios";
+import { ref } from "vue";
+
+const title = ref("");
+const content = ref("");
+
+const submitForm = async (event) => {
+  event.preventDefault();
+  if (!title.value || !content.value) {
+    alert("Please fill in all fields");
+    return;
+  }
+  try {
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/posts",
+      {
+        title: title.value,
+        content: content.value,
+      }
+    );
+    alert("Blog post created successfully");
+  } catch (error) {
+    alert("An error occurred while creating the blog post");
+  }
+};
+</script>
+
 <template>
   <div class="flex items-center justify-center w-screen h-screen">
     <main class="flex flex-col w-full max-w-5xl">
-      <form class="flex flex-col gap-4">
+      <form @submit="submitForm" class="flex flex-col gap-4">
         <div class="flex flex-col gap-2">
           <label for="title">Blog Title :</label>
-          <input class="border" type="text" name="title" id="title" />
+          <input v-model="title" class="border" type="text" id="title" />
         </div>
         <div class="flex flex-col gap-2">
           <label for="content">Blog Content :</label>
-          <textarea class="border" type="text" name="content" id="content" />
+          <textarea v-model="content" class="border" id="content"></textarea>
         </div>
         <div class="flex justify-center">
           <button class="w-1/3 px-8 border" type="submit">Submit</button>
