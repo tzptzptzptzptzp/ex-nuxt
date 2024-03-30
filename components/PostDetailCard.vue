3dbf7a23bb2c677aaaa6da17acc9b64d0d35f239
tzptzptzptzptzp <git@tzp.pink>
2024-03-30 23:21:40 +0900

    ⚡️ コンポーネントに分離

---
+++ components/PostDetailCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+type PostType = {
+  userId: number;
+  title: string;
+  body: string;
+};
+const props = defineProps<{
+  post: PostType;
+}>();
+const { post } = toRefs(props);
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <h2 class="text-3xl font-bold">{{ post.title }}</h2>
+    <p>UserID : {{ post.userId }}</p>
+    <p>{{ post.body }}</p>
+    <div class="w-full text-center">
+      <NuxtLink to="/">- TOP -</NuxtLink>
+    </div>
+  </div>
+</template>
--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -11,14 +11,7 @@ const post = data.value;
 <template>
   <div class="flex items-center justify-center w-screen h-screen">
     <main class="flex flex-col w-full max-w-5xl">
-      <div class="flex flex-col gap-4">
-        <h2 class="text-3xl font-bold">{{ post.title }}</h2>
-        <p>UserID : {{ post.userId }}</p>
-        <p>{{ post.body }}</p>
-        <div class="w-full text-center">
-          <NuxtLink to="/">- TOP -</NuxtLink>
-        </div>
-      </div>
+      <PostDetailCard :post="post" />
     </main>
   </div>
 </template>
