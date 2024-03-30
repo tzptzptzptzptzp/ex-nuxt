16b0e4f1724edb5e0065538be6005a61f08e2117
tzptzptzptzptzp <git@tzp.pink>
2024-03-30 13:17:13 +0900

    ✨ 取得したデータを展開

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -6,15 +6,15 @@ const { data, error, pending } = await useFetch(
   `https://jsonplaceholder.typicode.com/posts/${id}`
 );
 const post = data.value;
-console.log(post);
 </script>
+
 <template>
   <div class="flex items-center justify-center w-screen h-screen">
     <main class="flex flex-col w-full max-w-5xl">
       <div class="flex flex-col gap-4">
-        <h2 class="text-3xl font-bold">Post Title</h2>
-        <p>UserID : 1</p>
-        <p>Post Content</p>
+        <h2 class="text-3xl font-bold">{{ post.title }}</h2>
+        <p>UserID : {{ post.userId }}</p>
+        <p>{{ post.body }}</p>
       </div>
     </main>
   </div>
