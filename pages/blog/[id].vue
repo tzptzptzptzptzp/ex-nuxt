251fdf435dbf4ba60a469cf781a1069f69254f73
tzptzptzptzptzp <git@tzp.pink>
2024-03-30 13:10:09 +0900

    💄 詳細ページの雛形を作成

---
+++ pages/blog/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center justify-center w-screen h-screen">
+    <main class="flex flex-col w-full max-w-5xl">
+      <div class="flex flex-col gap-4">
+        <h2 class="text-3xl font-bold">Post Title</h2>
+        <p>UserID : 1</p>
+        <p>Post Content</p>
+      </div>
+    </main>
+  </div>
+</template>
