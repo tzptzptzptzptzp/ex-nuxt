7c96ee3cf72f0ac8dfe60fee02d2c50115fff85a
tzptzptzptzptzp <git@tzp.pink>
2024-03-30 13:16:51 +0900

    ✨ IDに応じたデータを取得

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -1,3 +1,13 @@
+<script setup>
+import { useRoute } from "vue-router";
+const route = useRoute();
+const id = route.params.id;
+const { data, error, pending } = await useFetch(
+  `https://jsonplaceholder.typicode.com/posts/${id}`
+);
+const post = data.value;
+console.log(post);
+</script>
 <template>
   <div class="flex items-center justify-center w-screen h-screen">
     <main class="flex flex-col w-full max-w-5xl">
