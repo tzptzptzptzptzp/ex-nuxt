8e1a7c0deeca7f6c2ddcfd0336a9bae191d28ad2
tzptzptzptzptzp <git@tzp.pink>
2024-04-02 22:19:07 +0900

    ✨ 編集ページの雛形を作成

---
+++ pages/blog/edit/[id].vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="flex items-center justify-center w-screen h-screen">
+    <main class="flex flex-col w-full max-w-5xl">edit</main>
+  </div>
+</template>
