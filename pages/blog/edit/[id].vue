0875c62b13beba8cbd695dd74fa03008eaceabd6
tzptzptzptzptzp <git@tzp.pink>
2024-04-03 22:45:15 +0900

    ✨ 初期値を指定

--- pages/blog/edit/[id].vue
+++ pages/blog/edit/[id].vue
@@ -1,7 +1,37 @@
-<script setup></script>
+<script setup>
+import { useRoute } from "vue-router";
+const route = useRoute();
+const id = route.params.id;
+const { data, error, pending } = await useFetch(
+  `https://jsonplaceholder.typicode.com/posts/${id}`
+);
+const post = data.value;
+const title = ref("");
+const content = ref("");
+
+watchEffect(() => {
+  if (!post) return;
+  title.value = post.title;
+  content.value = post.body;
+});
+</script>
 
 <template>
   <div class="flex items-center justify-center w-screen h-screen">
-    <main class="flex flex-col w-full max-w-5xl">edit</main>
+    <main class="flex flex-col w-full max-w-5xl">
+      <form class="flex flex-col gap-4">
+        <div class="flex flex-col gap-2">
+          <label for="title">Blog Title :</label>
+          <input v-model="title" class="border" type="text" id="title" />
+        </div>
+        <div class="flex flex-col gap-2">
+          <label for="content">Blog Content :</label>
+          <textarea v-model="content" class="border" id="content"></textarea>
+        </div>
+        <div class="flex justify-center">
+          <button class="w-1/3 px-8 border" type="submit">Submit</button>
+        </div>
+      </form>
+    </main>
   </div>
 </template>
